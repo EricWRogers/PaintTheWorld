eecc9ce2dc09d2d7b992cf9f3f765292801ede0b
dylanHilliard <ghilliard69420@gmail.com>
2025-09-15 11:57:22 -0500

    player movment with camera works no up or down tho

--- Assets/Scripts/ThirdPersonMovment.cs
+++ Assets/Scripts/ThirdPersonMovment.cs
@@ -3,10 +3,22 @@ using UnityEngine;
 public class ThirdPersonMovment : MonoBehaviour
 {
     public CharacterController controller;
+    public GameObject player;
     public Transform cam;
     public float speed = 6f;
     private bool isGrounded;
     public float gravity = 5.81f;
+
+    public float mouseSensitivity = 100f;
+    private Vector3 moveDir;
+    float turnSmoothVelocity;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        player = this.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
 
 
@@ -21,11 +33,30 @@ public class ThirdPersonMovment : MonoBehaviour
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
         
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, 0.1f);
+            //transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(speed * Time.deltaTime * moveDir.normalized);
+        }
+
+    
+
+
+    
+
+
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            transform.Rotate(Vector3.up * mouseX);
 
      
-            controller.Move(direction * speed * Time.deltaTime);
     
     }
 }
