d0f16fc459693ecdbc80d595ac9704828232e09a
dylanHilliard <ghilliard69420@gmail.com>
2025-09-15 16:26:00 -0500

    basic camera and movement works

--- Assets/Scripts/ThirdPersonMovment.cs
+++ Assets/Scripts/ThirdPersonMovment.cs
@@ -38,19 +38,14 @@ public class ThirdPersonMovment : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
+            //float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, 0.1f);
-            //transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothVelocity,0.1f);
+            
             moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(speed * Time.deltaTime * moveDir.normalized);
         }
-
-    
-
-
-    
-
 
 
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
