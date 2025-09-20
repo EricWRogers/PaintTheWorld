a31832ca660f916b8561a3e9659287b2453537b3
DylanHilliard <ghilliard69420@gmail.com>
2025-09-20 16:15:53 -0500

    new movment engine working

---
+++ Assets/Scripts/PlayerMovment/PlayerMovement.cs
@@ -0,0 +1,116 @@
+using System.ComponentModel.Design.Serialization;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace KinematicCharacterControler
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public float speed = 5f;
+        public float rotationSpeed = 5f;
+        public float maxWalkAngle = 60f;
+        public GameObject player;
+
+
+        private Transform m_orientation;
+        public Transform cam;
+
+        public  PlayerMovmentEngine engine;
+
+        public Vector3 gravity = new Vector3(0, -9, 0);
+        private float m_elapsedFalling;
+
+        private Vector3 m_velocity;
+
+
+        [Header("Jump Settings")]
+        public float jumpVelocity = 5.0f;
+        public float maxJumpAngle = 80f;
+        public float jumpCooldown = 0.25f;
+
+        public float jumpInputElapsed = Mathf.Infinity;
+        private float m_timeSinceLastJump = 0.0f;
+        public bool m_jumpInputPressed = false;
+        private float m_jumpBufferTime = 0.25f;
+
+        
+
+        
+
+        void Start()
+        {
+            player = GameObject.Find("Player");
+            m_orientation = cam;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
+                m_jumpInputPressed = true;
+            else
+                m_jumpInputPressed = false;
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+
+            Vector3 viewDir = transform.position - new Vector3(cam.position.x, transform.position.y, cam.position.z);
+            m_orientation.forward = viewDir.normalized;
+
+            Vector3 inputDir = m_orientation.forward * vertical + m_orientation.right * horizontal;
+
+            // rotate player
+            if (inputDir != Vector3.zero)
+            {
+                player.transform.forward = Vector3.Slerp(player.transform.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            }
+
+             bool onGround= engine.CheckIfGrounded(out RaycastHit groundHit);
+
+
+            bool falling = !(onGround && maxWalkAngle >= Vector3.Angle(Vector3.up, groundHit.normal));
+            print(falling);
+
+            if (falling)
+            {
+                m_velocity += gravity * Time.deltaTime;
+                m_elapsedFalling += Time.deltaTime;
+            }
+            else
+            {
+                m_velocity = Vector3.zero;
+                m_elapsedFalling = 0;
+            }
+
+
+            if (m_jumpInputPressed)
+            {
+                jumpInputElapsed = 0.0f;
+            }
+
+            // Player can jump if they are (1) on the ground, (2) within the ground jump angle, (3), after the jump cooldown
+            bool canJump = onGround  && engine.groundedState.angle <= maxJumpAngle && m_timeSinceLastJump >= jumpCooldown;
+            bool attemptingJump = jumpInputElapsed <= m_jumpBufferTime;
+
+            // Have player jump if they can jump and are attempting to jump
+            if (canJump && attemptingJump)
+            {
+                m_velocity = Vector3.up * jumpVelocity;
+                m_timeSinceLastJump = 0.0f;
+                jumpInputElapsed = Mathf.Infinity;
+                engine.shouldSnapDown = false;
+
+            }
+            else
+            {
+                m_timeSinceLastJump += Time.deltaTime;
+                jumpInputElapsed += Time.deltaTime;
+                engine.shouldSnapDown = true;
+            }
+
+            transform.position = engine.MovePlayer(inputDir * speed * Time.deltaTime);
+            transform.position = engine.MovePlayer(m_velocity * Time.deltaTime);
+   
+
+        }
+    }
+}
--- Assets/Scripts/PlayerMovment/PlayerMovmentEngine.cs
+++ Assets/Scripts/PlayerMovment/PlayerMovmentEngine.cs
@@ -1,69 +1,264 @@
+using System.Security.Cryptography.X509Certificates;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMovmentEngine : MonoBehaviour
+
+namespace KinematicCharacterControler
 {
-
-    public float defaultGroundCheck = 0.25f;
-    public float defaultGroundedDistance = 0.05f;
-    public Vector3 previousPosition = Vector3.zero;
-
-    public GroundedState groundedState { get;  protected set; }
-
-
-    void Awake()
+    [RequireComponent(typeof(CapsuleCollider))]
+    public class PlayerMovmentEngine : MonoBehaviour
     {
-        previousPosition = this.transform.position;
-    }
+          public CapsuleCollider capsule;
+        public LayerMask collisionLayers;
+
+        [Header("Collision & Slope")]
+        public float skinWidth = 0.015f;
+        public int maxBounces = 5;
+        public float maxSlopeAngle = 55f;
+        private float m_anglePower = 0.5f;
+
+        [Header("Ground Checks")]
+        public float defaultGroundCheck = 0.25f;
+        public float defaultGroundedDistance = 0.05f;
+        public float snapDownDistance = 0.45f; 
+        public bool shouldSnapDown = true; // Should snap to ground
+
+        [Header("Gravity (used only if not snapped)")]
+        public Vector3 gravity = new Vector3(0f, -9.81f, 0f);
+
+        public GroundedState groundedState { get; protected set; }
+
+        private Bounds bounds;
 
 
-    public void Move()
-    {
+        void Awake()
+        {
+            
+        }
+        /*
+        public void Move(Vector3 inputMove)
+        {
 
-    }
+            bool onGround = CheckIfGrounded();
 
+            Vector3 horizDelta = CollideAndSlide(inputMove, transform.position, 0, false, inputMove);
 
-    public void CheckCollisions()
-    {
+            Vector3 newPos = transform.position + horizDelta;
+            SnapPlayerDown();
+
+            transform.position = newPos;
+        }
+        */
+        public Vector3 MovePlayer(Vector3 movement)
+        {
         
+            Vector3 position = transform.position;
+            Quaternion rotation = transform.rotation;
 
-    }
+            Vector3 remaining = movement;
 
-    public bool CheckIfGrounded()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, defaultGroundCheck))
-        {
-            float angle = Vector3.Angle(hit.normal, Vector3.up);
-            bool isGrounded = hit.distance <= defaultGroundedDistance;
-            groundedState = new GroundedState(hit.distance, isGrounded, angle, hit.normal, hit.point);
-            return isGrounded;
+            int bounces = 0;
+
+            while (bounces < maxBounces && remaining.magnitude > 0.001f)
+            {
+                // Do a cast of the collider to see if an object is hit during this
+                // movement bounce
+                float distance = remaining.magnitude;
+                if (!CastSelf(position, rotation, remaining.normalized, distance, out RaycastHit hit))
+                {
+                    // If there is no hit, move to desired position
+                    position += remaining;
+
+                    // Exit as we are done bouncing
+                    break;
+                }
+
+                // If we are overlapping with something, just exit.
+                if (hit.distance == 0)
+                {
+                    break;
+                }
+
+                float fraction = hit.distance / distance;
+
+                // Set the fraction of remaining movement (minus some small value)
+                position += remaining * (fraction);
+                // Push slightly along normal to stop from getting caught in walls
+                position += hit.normal * 0.001f * 2;
+                // Decrease remaining movement by fraction of movement remaining
+                remaining *= (1 - fraction);
+
+                // Plane to project rest of movement onto
+                Vector3 planeNormal = hit.normal;
+
+                // Only apply angular change if hitting something
+                // Get angle between surface normal and remaining movement
+                float angleBetween = Vector3.Angle(hit.normal, remaining) - 90.0f;
+
+                // Normalize angle between to be between 0 and 1
+                // 0 means no angle, 1 means 90 degree angle
+                angleBetween = Mathf.Min(60f, Mathf.Abs(angleBetween));
+                float normalizedAngle = angleBetween / 60f;
+
+                // Reduce the remaining movement by the remaining movement that ocurred
+                remaining *= Mathf.Pow(1 - normalizedAngle, m_anglePower) * 0.9f + 0.1f;
+
+                // Rotate the remaining movement to be projected along the plane of the hit surface
+                Vector3 projected = Vector3.ProjectOnPlane(remaining, planeNormal).normalized * remaining.magnitude;
+
+                // If projected remaining movement is less than original remaining movement (so if the projection broke
+                // due to float operations), then change this to just project along the vertical.
+                if (projected.magnitude + 0.001f < remaining.magnitude)
+                {
+                    remaining = Vector3.ProjectOnPlane(remaining, Vector3.up).normalized * remaining.magnitude;
+                }
+                else
+                {
+                    remaining = projected;
+                }
+
+                // Track number of times the character has bounced
+                bounces++;
+            }
+
+            // We're done, player was moved as part of loop
+            return position;
         }
-        else
+
+        public Vector3 CollideAndSlide(Vector3 _vel, Vector3 _pos, int _depth, bool _gravityPass, Vector3 _velInit)
         {
-            groundedState = new GroundedState(defaultGroundCheck, false, 0f, Vector3.up, Vector3.zero);
-            return false;
+            if (_depth > maxBounces) return Vector3.zero;
+
+            float dist = _vel.magnitude + skinWidth;
+            bounds = capsule.bounds;
+            bounds.Expand(-2 * skinWidth);
+
+            RaycastHit hit;
+
+
+            if (CastSelf(_pos, transform.rotation, _vel.normalized, dist, out hit))
+            {
+                Vector3 snapToSurface = _vel.normalized * (hit.distance - skinWidth);
+                Vector3 leftOver = _vel - snapToSurface;
+                float angle = Vector3.Angle(Vector3.up, hit.normal);
+
+                if (snapToSurface.magnitude <= skinWidth) snapToSurface = Vector3.zero;
+
+                // SLope
+                if (angle <= maxSlopeAngle)
+                {
+                    if (_gravityPass)
+                    {
+                        return snapToSurface;
+                    }
+                    leftOver = ProjectAndScale(leftOver, hit.normal);
+                }
+                else  // Wall or steep slope
+                {
+                    float scale = 1 - Vector3.Dot(
+                        new Vector3(hit.normal.x, 0, hit.normal.z).normalized,
+                        -new Vector3(_velInit.x, 0, _velInit.z).normalized
+
+                    );
+
+                    if (groundedState.isGrounded && !_gravityPass)
+                    {
+                        leftOver = ProjectAndScale(
+                            new Vector3(leftOver.x, 0, leftOver.z),
+                            new Vector3(hit.normal.x, 0, hit.normal.z)
+                        ).normalized;
+                        leftOver *= scale;
+                    }
+                    else
+                    {
+                        leftOver = ProjectAndScale(leftOver, hit.normal) * scale;
+                    }
+                }
+
+                return snapToSurface + CollideAndSlide(leftOver, _pos + snapToSurface, _depth + 1, _gravityPass, _velInit);
+            }
+
+
+            return _vel;
         }
-    }
 
-
-    public struct GroundedState
-    { 
-        public float distToGround;
-        public bool isGrounded;
-        public float angle;
-        public Vector3 groundNormal;
-        public Vector3 groundHitPosition;
-   
- 
-     
-        public GroundedState(float distToGround, bool isGrounded, float angle, Vector3 groundNormal, Vector3 groundHitPosition)
+        private Vector3 ProjectAndScale(Vector3 _vec, Vector3 _normal)
         {
-            this.distToGround = distToGround;
-            this.isGrounded = isGrounded;
-            this.angle = angle;
-            this.groundNormal = groundNormal;
-            this.groundHitPosition = groundHitPosition;
+            float mag = _vec.magnitude;
+            _vec = Vector3.ProjectOnPlane(_vec, _normal).normalized;
+            _vec *= mag;
+            return _vec;
         }
 
+        public bool CheckIfGrounded(out RaycastHit _hit)
+        {
+
+            
+            if (CastSelf(transform.position, transform.rotation, Vector3.down, defaultGroundCheck, out _hit))
+            {
+                float angle = Vector3.Angle(_hit.normal, Vector3.up);
+                bool isGrounded = _hit.distance <= (capsule.height / 2f) + defaultGroundedDistance;
+
+                groundedState = new GroundedState(_hit.distance, isGrounded, angle, _hit.normal, _hit.point);
+                return isGrounded;
+                
+            }
+            else
+            {
+                groundedState = new GroundedState(defaultGroundCheck, false, 0f, Vector3.up, Vector3.zero);
+                return false;
+            }
+         }
+
+        public bool CastSelf(Vector3 _position, Quaternion _rot, Vector3 _direction, float _distance, out RaycastHit _hit)
+        {
+            // Use capsule collider’s true world position and radius
+            Vector3 center =  capsule.transform.position + capsule.center;
+            float halfHeight = (capsule.height / 2f) - capsule.radius;
+
+            Vector3 top =  center +  Vector3.up * halfHeight;
+            Vector3 bottom =  center + Vector3.down * halfHeight;
+            
+
+            return Physics.CapsuleCast(top, bottom, capsule.radius - skinWidth, _direction, out _hit, _distance, collisionLayers);
+        }
+
+        public void SnapPlayerDown()
+        {
+             bool closeToGround = CastSelf(
+                transform.position,
+                transform.rotation,
+                Vector3.down,
+                snapDownDistance,
+                out RaycastHit groundHit);
+
+            // If within the threshold distance of the ground
+            if (closeToGround && groundHit.distance > 0)
+            {
+                print("snapping down ");
+                // Snap the player down the distance they are from the ground
+                transform.position += Vector3.down * (groundHit.distance - 0.001f);
+            }
+        }
+       [System.Serializable]
+        public struct GroundedState
+        {
+            public float distToGround;
+            public bool isGrounded;
+            public float angle;
+            public Vector3 groundNormal;
+            public Vector3 groundHitPosition;
+
+            public GroundedState(float distToGround, bool isGrounded, float angle, Vector3 groundNormal, Vector3 groundHitPosition)
+            {
+                this.distToGround = distToGround;
+                this.isGrounded = isGrounded;
+                this.angle = angle;
+                this.groundNormal = groundNormal;
+                this.groundHitPosition = groundHitPosition;
+            }
+
+        }
     }
 }
