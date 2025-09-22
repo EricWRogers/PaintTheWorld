90348caad5961f82fbef1339122ae1f6fb111073
Aidan M. <aidansmcmurry@gmail.com>
2025-09-22 17:12:08 -0500

    Added stable Id's for each item and made a game events script

---
+++ Assets/Scripts/ItemsAndEvents/ItemIds.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+
+public static class ItemIds
+{
+    public const string PaintLauncher   = "paint_launcher";
+    public const string ReactiveArmor   = "reactive_armor";
+    public const string ExplosivePaint  = "explosive_paint";
+    public const string SiphoningPaint  = "siphoning_paint";
+    public const string GiftOfLife      = "gift_of_life";
+    public const string FountainOfYouth = "fountain_of_youth";
+}
