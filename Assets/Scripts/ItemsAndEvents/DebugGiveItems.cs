e6b9d046b8cc212be679314dafabe1dfdebd1096
AidanPC <aidansmcmurry@gmail.com>
2025-09-24 12:09:11 -0500

    Added debug scripts to test items

--- Assets/Scripts/ItemsAndEvents/DebugGiveItems.cs
+++ Assets/Scripts/ItemsAndEvents/DebugGiveItems.cs
@@ -1,16 +1,30 @@
+// DebugGiveItems.cs
 using UnityEngine;
 
 public class DebugGiveItems : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Inventory inv;
+    public Currency wallet;
+    public ItemDefinition paintLauncher, reactiveArmor, explosivePaint, siphoningPaint, giftOfLife, fountain;
+
+    private void Awake()
     {
-        
+        if (!inv) inv = FindObjectOfType<Inventory>();
+        if (!wallet) wallet = FindObjectOfType<Currency>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!inv) return;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && paintLauncher != null)    inv.Add(paintLauncher, 1);
+        if (Input.GetKeyDown(KeyCode.Alpha2) && reactiveArmor != null)    inv.Add(reactiveArmor, 1);
+        if (Input.GetKeyDown(KeyCode.Alpha3) && explosivePaint != null)   inv.Add(explosivePaint, 1);
+        if (Input.GetKeyDown(KeyCode.Alpha4) && siphoningPaint != null)   inv.Add(siphoningPaint, 1);
+        if (Input.GetKeyDown(KeyCode.Alpha5) && giftOfLife != null)       inv.Add(giftOfLife, 1);
+        if (Input.GetKeyDown(KeyCode.Alpha6) && fountain != null)         inv.Add(fountain, 1);
+
+        if (Input.GetKeyDown(KeyCode.G) && wallet != null) wallet.Add(100);
     }
 }
+
