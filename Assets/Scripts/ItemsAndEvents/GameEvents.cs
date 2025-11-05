56118b9b8bc9012cb224175c1c82dec0ddc7c6e9
AidanPC <aidansmcmurry@gmail.com>
2025-11-04 20:45:06 -0600

    Grind burst paint also works for wall running

--- Assets/Scripts/ItemsAndEvents/GameEvents.cs
+++ Assets/Scripts/ItemsAndEvents/GameEvents.cs
@@ -16,10 +16,16 @@ public static class GameEvents
     public static Action<int> PlayerHealed;                         // heal player
     public static Action<GameObject> EnemyKilled;                   // enemy gameobjectt
 
+    public static event Action WallRunStarted;
+    public static event Action WallRunTick;
+
     public static System.Action PlayerDodged;                     // fires when a dash begins
     public static System.Action PlayerStartedGrinding;            // fires when rail grind starts
     public static System.Action PlayerGrindingTick;               // fires periodically while grinding
 
     // Paint progress, call when we use paint
     public static System.Action<float> PaintApplied;              // amount added, add units later
+
+    public static void RaiseWallRunStarted() => WallRunStarted?.Invoke();
+    public static void RaiseWallRunTick()    => WallRunTick?.Invoke();
 }
--- Assets/Scripts/PlayerMovment/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovment/PlayerMovement.cs
@@ -475,14 +475,16 @@ public class PlayerMovement : PlayerMovmentEngine
                 {
                     m_isWallRiding = true;
                     m_wallNormal = hit.normal;
-                    if (leftWall)
-                    {
-                        paintRotation -= 90;
-                    }
-                    else if(rightWall)
-                    {
-                        paintRotation += 90;
-                    }
+                if (leftWall)
+                {
+                    paintRotation -= 90;
+                }
+                else if (rightWall)
+                {
+                    paintRotation += 90;
+                }
+                    
+                GameEvents.RaiseWallRunStarted();
                 }
                 paintPoint.Rotate(0, 0f, paintRotation);
             
@@ -511,7 +513,7 @@ public class PlayerMovement : PlayerMovmentEngine
                     m_velocity = m_wallRunDir * currSpeed + -Vector3.up * wallGravity;
                 }
 
-
+                GameEvents.RaiseWallRunTick();
 
                 return true;
             }
