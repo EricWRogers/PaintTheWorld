c25d4133cca69296de037847a6224270599fe765
ItsCrasyGamer_YT <potatoe0201@gmail.com>
2026-03-16 14:50:30 -0500

    Updated Subway Spawn Script

--- Assets/SubwaySpawner.cs
+++ Assets/SubwaySpawner.cs
@@ -1,9 +1,46 @@
 using UnityEngine;
+using SuperPupSystems.Helper; 
 
 public class SubwaySpawner : MonoBehaviour
 {
+    public Timer targetTimer;
     public GameObject prefabToSpawn;
-    public void Spawn() {
-        Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+
+    private bool _hasSpawnedThisCycle = false;
+    private float _lastStartTime;
+
+    void Start()
+    {
+        if (targetTimer == null)
+            targetTimer = GetComponent<Timer>();
+            
+        if (targetTimer != null)
+            _lastStartTime = targetTimer.countDownTime;
+    }
+
+    void Update()
+    {
+        if (targetTimer == null) return;
+
+        float currentTime = targetTimer.timeLeft;
+
+        if (currentTime > _lastStartTime * 0.9f) 
+        {
+            _hasSpawnedThisCycle = false;
+        }
+
+        if (!_hasSpawnedThisCycle && currentTime > 0 && currentTime <= (targetTimer.countDownTime * 0.5f))
+        {
+            Spawn();
+            _hasSpawnedThisCycle = true;
+        }
+    }
+
+    public void Spawn() 
+    {
+        if (prefabToSpawn != null)
+        {
+            Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+        }
     }
 }
