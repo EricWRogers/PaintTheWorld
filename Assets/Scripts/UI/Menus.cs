4bd18564d54b688621c655021d4641eafcd936da
AidanPC <aidansmcmurry@gmail.com>
2026-04-05 19:50:54 -0500

    Fixed Settings Tab

--- Assets/Scripts/UI/Menus.cs
+++ Assets/Scripts/UI/Menus.cs
@@ -4,19 +4,46 @@ using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour
 {
+    [Header("Scene")]
     public string sceneToLoad;
+
+    [Header("Pause Menu Panels")]
+    public GameObject mainButtonsPanel;
+    public GameObject settingsPanel;
+
     void Start()
     {
         GameManager.instance.PauseGame();
+
+        if (mainButtonsPanel != null)
+            mainButtonsPanel.SetActive(true);
+
+        if (settingsPanel != null)
+            settingsPanel.SetActive(false);
     }
+
     public void PlayGame()
     {
         GameManager.instance.ResumeGame();
         SceneManager.LoadScene(sceneToLoad);
     }
-    public void VolumeScroller(GameObject _pauseHud)
-    {
 
+    public void OpenSettings()
+    {
+        if (mainButtonsPanel != null)
+            mainButtonsPanel.SetActive(false);
+
+        if (settingsPanel != null)
+            settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        if (settingsPanel != null)
+            settingsPanel.SetActive(false);
+
+        if (mainButtonsPanel != null)
+            mainButtonsPanel.SetActive(true);
     }
 
     public void Resume(GameObject _pauseHud)
@@ -39,6 +66,5 @@ public class Menus : MonoBehaviour
         #else
         Application.Quit();
         #endif
-        
     }
 }
