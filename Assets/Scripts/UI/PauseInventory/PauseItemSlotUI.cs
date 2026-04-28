bc223e525dd476aeb09aac64d78702031998187b
AidanPC <aidansmcmurry@gmail.com>
2026-04-28 16:50:58 -0500

    Added stack count to inventory

--- Assets/Scripts/UI/PauseInventory/PauseItemSlotUI.cs
+++ Assets/Scripts/UI/PauseInventory/PauseItemSlotUI.cs
@@ -6,16 +6,21 @@ using TMPro;
 public class PauseItemSlotUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Image iconImage;
-    
+
+    [Header("Stack Count UI")]
+    public TextMeshProUGUI stackCountText;
+    public GameObject stackCountBackground;
 
     private ItemSO item;
     private bool owned;
+    private int stackCount;
     private PauseItemTooltip tooltip;
 
-    public void Setup(ItemSO newItem, bool isOwned, PauseItemTooltip tooltipRef)
+    public void Setup(ItemSO newItem, bool isOwned, int count, PauseItemTooltip tooltipRef)
     {
         item = newItem;
         owned = isOwned;
+        stackCount = count;
         tooltip = tooltipRef;
 
         if (iconImage != null)
@@ -23,6 +28,28 @@ public class PauseItemSlotUI : MonoBehaviour, IPointerEnterHandler, IPointerExit
             iconImage.sprite = item != null ? item.icon : null;
             iconImage.enabled = item != null && item.icon != null;
             iconImage.color = owned ? Color.white : new Color(0.4f, 0.4f, 0.4f, 1f);
+        }
+
+        UpdateStackCountUI();
+    }
+
+    private void UpdateStackCountUI()
+    {
+        bool showStackUI = stackCount > 0;
+
+        if (stackCountText != null)
+        {
+            stackCountText.gameObject.SetActive(showStackUI);
+
+            if (showStackUI)
+            {
+                stackCountText.text = stackCount.ToString();
+            }
+        }
+
+        if (stackCountBackground != null)
+        {
+            stackCountBackground.SetActive(showStackUI);
         }
     }
 
